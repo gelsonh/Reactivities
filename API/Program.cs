266727c93b78259d62da40f4aeb13d34e3349ffa
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-12-27 01:05:45 -0500

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Creates a service scope using the app's service container.
+// Common in ASP.NET Core with Dependency Injection (DI) to manage service instances.
+// The scope is used to define the lifespan of service instances. 'using' ensures
+// proper resource cleanup when the scope is no longer needed.
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+   var logger = services.GetRequiredService<ILogger<Program>>();
+   logger.LogError(ex, "An error occured during migration");
+}
+app.Run();
